ea55efc5f14ede7da2e83520a793e4137d639ba4
Md Ruhul Amin <ra.01403288711@gmail.com>
2022-03-06 11:02:32 +0600

    class component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import React,{Component} from 'react';
 import Greet from './components/Greet';
 import Greet2 from './components/Greet2';
 import {Greet3} from './components/Greet3';
+import Welcome from './components/Welcome';
+import Welcome2 from './components/Welcome2';
 class App extends Component {
   render(){
     return(
       <div className='App'>
+        <Welcome2/>
        <Greet/>
        <Greet2/>
        <Greet3/>
+       <Welcome/>
       </div>
     )
   }
 }
-
 export default App;
---
+++ src/components/Welcome2.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+
+class Welcome2 extends Component{
+    render(){
+        return <h1>Welcome 2 from Class Component! </h1>
+    }
+
+
+
+}
+
+export default Welcome2;
